9ebc603637b3246d2e7c891882bea2d23fc2e0fc
ffoff <ffoff@XPOWERLAPTOP>
2022-11-09 17:49:05 +0100

    Aggiungere i file di progetto.

---
+++ GGGiver/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("wanna get a GG?");
+        string gg = Console.ReadLine();
+        if (gg == "yes") { Console.WriteLine("gg"); }
+        else if (gg == "y") { Console.WriteLine("gg"); }
+        else { Console.WriteLine("lol you are not getting your GG, restart the program and try again :)"); }
+    }
+}
